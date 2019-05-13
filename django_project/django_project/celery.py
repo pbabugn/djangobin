f33be5d5fe39a38325ea79b39679cebd6b000a58
asdasd <bsdhbds@gmail.com>
2019-05-13 11:56:09 +0530

    fourth commit

--- django_project/django_project/celery.py
+++ django_project/django_project/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
@@ -15,3 +16,10 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'remove-snippets-daily-at-midnight': {
+        'task': 'djangobin.tasks.remove_snippets',
+        'schedule': crontab(minute=0, hour=0),
+    },
+}
